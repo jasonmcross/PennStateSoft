15bd987322354887f5147de681ec8f232b403593
Robert Myers <robbamyers@icloud.com>
2023-07-23 03:42:13 -0400

    Home, Meeting Create, Profile, File Complaint Client screens

--- app/client/client.js
+++ app/client/client.js
@@ -1,28 +1,193 @@
-document.getElementById('login-form').addEventListener('submit', async function(event) {
+// your_single_js_file.js
+
+// Function to handle logout
+async function handleLogout(event) {
   event.preventDefault();
 
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
+  fetch('/logout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => {
+    if (response.status === 200) {
+      window.location.href = '/';
+    }
+  });
+}
+
+async function handleHome(event) {
+  event.preventDefault();
+
+  fetch('/home', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => {
+    if (response.status === 200) {
+      window.location.href = '/home';
+    }
+  });
+}
+
+async function handleRegister(event) {
+  event.preventDefault();
 
   var encoder = new TextEncoder();
-  var data = encoder.encode(password);
+  var data = encoder.encode(document.getElementById('password').value);
   var hash = await window.crypto.subtle.digest('SHA-256', data);
   var hashedPassword = Array.from(new Uint8Array(hash)).map(b => b.toString(16).padStart(2, '0')).join('');
 
-  fetch('/login', {
+  let body = {
+    firstName: document.getElementById('firstName').value,
+    lastName: document.getElementById('lastName').value,
+    username: document.getElementById('username').value,
+    password: hashedPassword,
+    type: "client",
+  }
+
+  fetch('/register', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({
-      username: username,
-      password: hashedPassword
-    })
-  }).then( response => {
-    if(response.status === 200) {
+    body: JSON.stringify(body),
+  }).then(response => {
+    if (response.status === 200) {
       window.location.href = '/home';
-    } else {
-      document.getElementById('error-message').innerText = 'Invalid username or password';
+    } else if (response.status === 401) {
+      document.getElementById('error-message').innerHTML = 'Username already exists';
     }
-  })
-});
+  });
+}
+
+if(window.location.pathname === '/register') {
+  document.getElementById('registration-form').addEventListener('submit', handleRegister);
+}
+
+// Check the current page and add the logout event listener if on the dashboard page
+if (window.location.pathname === '/home') {
+  document.getElementById('logout').addEventListener('click', handleLogout);
+  let meetings = JSON.parse(localStorage.getItem("meetings"));
+  for(let i = 0; i < meetings.length; i++) {
+    let meeting = meetings[i];
+    let table = document.getElementById('meeting-table');
+    let row = table.insertRow();
+    let meetingName = row.insertCell(0);
+    let meetingDate = row.insertCell(1);
+    let meetingTime = row.insertCell(2);
+    let meetingRoom = row.insertCell(3);
+    let attendees = row.insertCell(4);
+    let type = row.insertCell(5);
+    meetingName.innerHTML = meeting.meetingName;
+    meetingDate.innerHTML = meeting.meetingDate;
+    meetingTime.innerHTML = meeting.meetingTime;
+    meetingRoom.innerHTML = meeting.meetingRoom;
+    attendees.innerHTML = meeting.attendees;
+    type.innerHTML = meeting.type;
+  }
+}
+
+if (window.location.pathname === '/client-profile') {
+  console.log(localStorage.getItem("firstName"));
+  console.log(localStorage.getItem("lastName"));
+  console.log(localStorage.getItem("type"));
+  document.getElementById('logout').addEventListener('click', handleLogout);
+  document.getElementById("firstName").innerHTML = localStorage.getItem("firstName");
+  document.getElementById("lastName").innerHTML = localStorage.getItem("lastName");
+}
+
+if (window.location.pathname === '/create-meeting') {
+  document.getElementById('create-meeting-form').addEventListener('submit', async function (event){
+    event.preventDefault();
+    let type = document.getElementsByName('room-type');
+    for(let i = 0; i < type.length; i++) {
+      if(type[i].checked) {
+        type = type[i].value;
+        break;
+      }
+    }
+    await fetch('/create-meeting', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        organizer: localStorage.getItem("username"),
+        meetingName: document.getElementById('meeting-name').value,
+        meetingDate: document.getElementById('date').value,
+        meetingTime: document.getElementById('time').value,
+        meetingRoom: document.getElementById('room').value,
+        attendees: document.getElementById('attendees').value,
+        type: type,
+      })
+    }).then(response => {
+      console.log(response);
+      if (response.status === 200) {
+        // add meeting to existing meetings in local storage
+        let meetings = JSON.parse(localStorage.getItem("meetings"));
+        let type = document.getElementsByName('room-type');
+        for(let i = 0; i < type.length; i++) {
+          if(type[i].checked) {
+            type = type[i].value;
+            break;
+          }
+        }
+        let newMeeting = {
+          organizer: localStorage.getItem("username"),
+          meetingName: document.getElementById('meeting-name').value,
+          meetingDate: document.getElementById('date').value,
+          meetingTime: document.getElementById('time').value,
+          meetingRoom: document.getElementById('room').value,
+          attendees: document.getElementById('attendees').value,
+          type: type,
+        };
+        meetings.push(newMeeting);
+        localStorage.setItem('meetings', JSON.stringify(meetings));
+        }
+    });
+  });
+}
+
+if(window.location.pathname === '/') {
+  document.getElementById('login-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    console.log('here');
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    var encoder = new TextEncoder();
+    var data = encoder.encode(password);
+    var hash = await window.crypto.subtle.digest('SHA-256', data);
+    var hashedPassword = Array.from(new Uint8Array(hash)).map(b => b.toString(16).padStart(2, '0')).join('');
+
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: username,
+        password: hashedPassword
+      })
+    }).then( response => {
+      if(response.status === 200) {
+        response.json().then(data => {
+          localStorage.setItem('username', username),
+          localStorage.setItem('firstName', data.firstName);
+          localStorage.setItem('lastName', data.lastName);
+          localStorage.setItem('type', data.type);
+          localStorage.setItem('meetings', JSON.stringify(data.meetings));
+          localStorage.setItem('attendee', JSON.stringify(data.attendee));
+          localStorage.setItem('complaints', JSON.stringify(data.complaints));
+        });
+        window.location.href = '/home';
+      } else {
+        document.getElementById('error-message').innerText = 'Invalid username or password';
+      }
+    })
+  });
+}
+
---
+++ server/server.js
@@ -0,0 +1,114 @@
+// Importing express module
+const express = require("express");
+const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');  // Importing cookie-parser
+const data = require('../database/data.json');  // Importing users.json
+const fs = require('fs');
+// Parse incoming JSON
+app.use(express.json());
+
+app.use(cookieParser());  // Use cookie-parser middleware
+
+// Used to bring the static CSS files into the server
+app.use(express.static(path.join(__dirname, '../app')));
+app.use(express.static(path.join(__dirname, '../database')));
+
+// Handling GET / request
+
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "../app/pages/login.html");
+  res.sendFile(filePath);
+});
+
+// Handling POST /login request
+app.post('/login', function(req, res) {
+  let username = req.body.username;
+  let hashedPassword = req.body.password;
+  let users = data['users'];
+  if(users[username] && users[username].password === hashedPassword) {
+    res.cookie('session', '1', { maxAge: 3600000, httpOnly: true });
+    let meetings = data.meetings.filter(meeting => meeting.organizer === username);
+    let attendee = data.meetings.filter(meeting => meeting.attendees.includes(username));
+    let complaints = data.complaints.filter(complaint => complaint.username === username);
+    // Send back the user's first name, last name, and type, along with all rooms, meetings where they are an organizer or in the list of attendees, or complaints they have file
+    res.json({ firstName: users[username].firstName, lastName: users[username].lastName, type: users[username].type, meetings: meetings, attendee: attendee, complaints: complaints });
+  } else {
+    res.status(401).json({ message: 'Invalid username or password' });
+  }
+});
+
+app.post('/register', function(req, res) {
+  console.log(req.body);
+  let username = req.body.username;
+  let hashedPassword = req.body.password;
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let type = req.body.type;
+  let users = data['users'];
+  if(users[username]) {
+    res.status(401).json({ message: 'Username already exists' });
+  } else {
+    let newData = data;
+    newData['users'][username] = {
+      username: username,
+      password: hashedPassword,
+      firstName: firstName,
+      lastName: lastName,
+      type: type
+    };
+    fs.writeFile(path.join(__dirname, '../database/data.json'), JSON.stringify(newData), function(err) {});
+    res.cookie('session', '1', { maxAge: 3600000, httpOnly: true });
+    res.json({ firstName: firstName, lastName: lastName, type: type, meetings: [], attendee: [], complaints: [] });
+  }
+});
+
+app.post('/logout', function(req, res) {
+  res.clearCookie('session');
+  res.json({ message: 'Logout successful' });
+});
+
+app.get('/home', function(req, res) {
+  if(req.cookies.session === '1') {
+    res.sendFile(path.join(__dirname, "../app/pages/home.html"));
+  }
+  else {
+    res.redirect('/');
+  }
+})
+
+app.get('/client-profile', function(req, res) {
+  if(req.cookies.session === '1') {
+    res.sendFile(path.join(__dirname, "../app/pages/client-profile.html"));
+  }
+})
+
+app.get('/create-meeting', function(req, res) {
+  if(req.cookies.session === '1') {
+    res.sendFile(path.join(__dirname, "../app/pages/create-meeting.html"));
+  }
+})
+
+app.post('/create-meeting', function(req, res) {
+  if(req.cookies.session === '1') {
+    let newData = data;
+    newData['meetings'].push(req.body);
+    fs.writeFile(path.join(__dirname, '../database/data.json'), JSON.stringify(newData), function(err) {});
+    res.json({ data: newData });
+  }
+});
+
+app.get('/file-complaint', function(req, res) {
+  if(req.cookies.session === '1') {
+    res.sendFile(path.join(__dirname, "../app/pages/file-complaint.html"));
+  }
+})
+
+app.get('/register', function(req, res) {
+  res.sendFile(path.join(__dirname, "../app/pages/registration.html"));
+});
+
+// Server setup
+app.listen(3000, () => {
+  console.log("Server is Running");
+});
